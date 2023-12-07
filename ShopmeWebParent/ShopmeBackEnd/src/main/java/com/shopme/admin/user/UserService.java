a38d7f94cbdda779e4a499b82d56a0e0d9e272fb
Aryan Kumar <aryankr730@gmail.com>
2023-12-07 18:20:31 +0530

    Added the encode user password method
    BCrypt is used to encrypt passwords

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -3,6 +3,7 @@ package com.shopme.admin.user;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.shopme.common.entity.Role;
@@ -11,21 +12,31 @@ import com.shopme.common.entity.User;
 @Service
 public class UserService {
 
-	@Autowired
-	private UserRepository userRepo;
+    @Autowired
+    private UserRepository userRepo;
 
-	@Autowired
-	private RoleRepository roleRepo;
+    @Autowired
+    private RoleRepository roleRepo;
 
-	public List<User> listAll() {
-		return (List<User>) userRepo.findAll();
-	}
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-	public List<Role> listRows() {
-		return (List<Role>) roleRepo.findAll();
-	}
+    public List<User> listAll() {
+        return (List<User>) userRepo.findAll();
+    }
+
+    public List<Role> listRows() {
+        return (List<Role>) roleRepo.findAll();
+    }
 
     public void save(User user) {
+        encodePassword(user);
         userRepo.save(user);
+    }
+
+    private void encodePassword(User user) {
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        
     }
 }
