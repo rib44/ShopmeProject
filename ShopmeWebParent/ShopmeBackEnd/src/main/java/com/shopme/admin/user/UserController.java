65b317c08c17761e6f4ba1281fc45a80939337ff
Aryan Kumar <aryankr730@gmail.com>
2023-12-21 14:43:40 +0530

    Added the option to upload user's photo to local storage

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -1,15 +1,20 @@
 package com.shopme.admin.user;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.shopme.admin.FileUploadUtil;
 import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
@@ -44,19 +49,30 @@ public class UserController {
     }
 
     @PostMapping("/users/save")
-    public String saveUser(User user, RedirectAttributes redirectAttributes) {
-        // REMOVE AFTER TESTING
-        System.out.println(user);
+    public String saveUser(User user, RedirectAttributes redirectAttributes,
+            @RequestParam("image") MultipartFile multipartFile)
+            throws IOException {
+        if (!multipartFile.isEmpty()) {
+            String fileName = StringUtils
+                    .cleanPath(multipartFile.getOriginalFilename());
+            user.setPhotos(fileName);
+            User savedUser = service.save(user);
 
-        service.save(user);
+            String uploadDir = "user-photos/" + savedUser.getId();
 
-        redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+            FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+        }
+
+        // service.save(user);
+        redirectAttributes.addFlashAttribute("message",
+                "The user has been saved successfully.");
 
         return "redirect:/users";
     }
 
     @GetMapping("/users/edit/{id}")
-    public String editUser(@PathVariable(name = "id") Integer id, Model model, RedirectAttributes redirectAttributes) {
+    public String editUser(@PathVariable(name = "id") Integer id, Model model,
+            RedirectAttributes redirectAttributes) {
         try {
             User user = service.get(id);
             List<Role> listRoles = service.listRows();
@@ -79,8 +95,9 @@ public class UserController {
 
         try {
             service.delete(id);
-            redirectAttributes.addFlashAttribute("message", "The user ID " + id + " has been deleted successfully");
-            
+            redirectAttributes.addFlashAttribute("message",
+                    "The user ID " + id + " has been deleted successfully");
+
         } catch (UserNotFoundException ex) {
             redirectAttributes.addFlashAttribute("message", ex.getMessage());
         }
@@ -89,7 +106,8 @@ public class UserController {
 
     @GetMapping("/users/{id}/enabled/{status}")
     public String updateUserEnabledStatus(@PathVariable(name = "id") Integer id,
-            @PathVariable(name = "status") boolean enabled, RedirectAttributes redirectAttributes) {
+            @PathVariable(name = "status") boolean enabled,
+            RedirectAttributes redirectAttributes) {
         service.updateEnabledStatus(id, enabled);
         String status = enabled ? "enabled" : "disabled";
         String message = "The user ID " + id + " has been " + status;
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -32,7 +32,7 @@ public class UserService {
         return (List<Role>) roleRepo.findAll();
     }
 
-    public void save(User user) {
+    public User save(User user) {
         boolean isUpdatingUser = (user.getId() != null);
 
         // for old users, to update
@@ -52,7 +52,7 @@ public class UserService {
             encodePassword(user);
         }
 
-        userRepo.save(user);
+        return userRepo.save(user);
     }
 
     private void encodePassword(User user) {
@@ -92,14 +92,16 @@ public class UserService {
         try {
             return userRepo.findById(id).get();
         } catch (NoSuchElementException ex) {
-            throw new UserNotFoundException("Could not find any user with ID " + id);
+            throw new UserNotFoundException(
+                    "Could not find any user with ID " + id);
         }
     }
 
     public void delete(Integer id) throws UserNotFoundException {
         Long countById = userRepo.countById(id);
         if (countById == null || countById == 0) {
-            throw new UserNotFoundException("Could not find any user with ID " + id);
+            throw new UserNotFoundException(
+                    "Could not find any user with ID " + id);
         }
 
         userRepo.deleteById(id);
