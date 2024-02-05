028bf1e4f2c1601509482896e66cca9d169287c9
Aryan Kumar <aryankr730@gmail.com>
2024-02-05 23:39:09 +0530

    WebSecurityConfig updated for login and because of SpringBoot 3.0

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -3,27 +3,56 @@ package com.shopme.admin.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class WebSecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
-    
+
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        // Allows no-password login
+//        http.authorizeHttpRequests(authz -> authz.anyRequest().permitAll());
+//        
+//        return http.build();
+//    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // Allows no-password login
-        http.authorizeHttpRequests(authz -> authz.anyRequest().permitAll());
+    	http.authorizeHttpRequests((authz) -> {
+                    try {
+                        authz.anyRequest().authenticated()
+                              .and()
+                              .formLogin(formLogin -> formLogin
+		                              .loginPage("/login")
+		                              .permitAll()
+                            		  );  
+                        
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    
+                });
+    	
         
         return http.build();
     }
     
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+//    	ignores theses things from the spring security, so could 
+//    	be displayed / fetched without logging in.
+    	return ((web) -> web.ignoring()
+    						.requestMatchers("/images/**", "/js/**", "/webjars/**")
+    			);
+    }
+
 }
